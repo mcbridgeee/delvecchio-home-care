8c50e84d1babcf2a469b3d327d4fa8d80640acc0
mcbridgeee <bridgetmctiernan@gmail.com>
2025-12-10 17:07:04 -0500

    feat: setup initial formatting of site

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-undef': 'error',
+    'no-console': 'off'
+  },
+  ignorePatterns: ['_site/**', 'dist/**', 'node_modules/**']
+};
---
+++ eleventy.config.js
@@ -0,0 +1,28 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+
+  // Add date filter
+  eleventyConfig.addFilter("date", (dateObj, format = "LLLL d, yyyy") => {
+    let date = dateObj;
+    if (date === "now") {
+      date = new Date();
+    }
+    if (!date) return "";
+    const jsDate = date instanceof Date ? date : new Date(date);
+    return DateTime.fromJSDate(jsDate, { zone: "utc" }).toFormat(format);
+  });
+
+  // Passthrough copy for CSS
+  eleventyConfig.addPassthroughCopy("src/style.css");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      layouts: "_includes/layouts",
+      output: "_site"
+    },
+    templateFormats: ["njk", "md", "html"]
+  };
+};
